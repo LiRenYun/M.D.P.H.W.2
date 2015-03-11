10512892d6599856884a31e68b20b5594cdd66b4
LiRenYun <ms0301735@gmail.com>
2015-03-11 22:04:34 +0800

    update

--- 1040305課堂練習/Classtry.java
+++ 1040305課堂練習/Classtry.java
@@ -1,18 +1,16 @@
-package pkg1040305classpractice;
 import java.util.Scanner;
 import static java.lang.System.*;
 import java.util.*;
 import java.lang.*;
-public class Main {
+public class Classtry{
     public static void main(String[] args) {
         String temp = null;
         String []tempStrings = null;
         ArrayList <String> stringArray = new ArrayList<String>(); 
-        //System.out.println(temp.matches("^[a-zA-Z]*"));
         while(true)
         {
-            Scanner enter = new Scanner(in);
-            out.printf("Enter the String-1)break:");
+            Scanner enter = new Scanner(System.in);
+            System.out.printf("Enter the String-1)break:");
             temp = enter.nextLine();
             if(temp.equals("-1") == true)
                 break;
@@ -20,11 +18,11 @@ public class Main {
             {
                 tempStrings = temp.split(",|\\.| |!|:|;");
                 for(int i = 0;i<tempStrings.length;i++)
-                    if(tempStrings[i].matches("^[a-zA-Z]*") == true && tempStrings[i].isEmpty() != true)    //正則表示法(Regular expression)
+                    if(tempStrings[i].matches("^[a-zA-Z]*") == true && tempStrings[i].isEmpty() != true)
                         stringArray.add(tempStrings[i].toLowerCase());
                 for(int i = 0;i<stringArray.size();i++)
-                    out.println(stringArray.get(i));
-                stringArray.removeAll(stringArray);//初始化陣列
+                    System.out.println(stringArray.get(i));
+                stringArray.removeAll(stringArray);
             }
         }
         
